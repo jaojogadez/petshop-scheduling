96fcc51d31c4c287f828408e47efb9a3a0ecd313
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-04-05 21:21:40 -0300

    feat: instalação do pacote dayjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 "use strict";
 
+// Configuração do dayjs
+import "./libs/dayjs.js";
+
 // CSS
 import "./styles/vars.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
+
+import dayjs from "dayjs";
